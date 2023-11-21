59b4e428644e4ca5e10133ad6a58a357f8d66e26
Bartłomiej Tempka <tempkabartek@gmail.com>
2023-11-21 18:09:06 +0100

    lab 4 zajecia

--- lab4/TypeChecker.py
+++ lab4/TypeChecker.py
@@ -1,4 +1,4 @@
-
+from TreePrinter import addToClass
 class NodeVisitor(object):
 
     def visit(self, node):
@@ -25,6 +25,9 @@ class NodeVisitor(object):
     #    for child in node.children:
     #        self.visit(child)
 
+@addToClass(AST.Node)
+def accept(self, visitor):
+    return visitor.visit(self)
 
 
 class TypeChecker(NodeVisitor):
@@ -35,9 +38,53 @@ class TypeChecker(NodeVisitor):
         type1 = self.visit(node.left)     # type1 = node.left.accept(self) 
         type2 = self.visit(node.right)    # type2 = node.right.accept(self)
         op    = node.op
-        # ... 
-        #
- 
+        if type1 == type2 and type1 in ['int', 'float']:
+            if op in ['+', '-', '*', '/']:
+                return type1
+            elif op in ['<', '>', '==', '!=']:
+                return 'bool'
+            else:
+                print("Unsupported operator")
+                return 'error'
+        elif (type1 == "int" and type2 == "float") or (type1 == "float" and type2 == "int"):
+            if op in ['+', '-', '*', '/']:
+                return 'float'
+            elif op in ['<', '>', '==', '!=']:
+                return 'bool'
+            else:
+                print("Unsupported operator")
+                return 'error'
+        elif type1 == type2 and type1 in ['bool']:
+            if op in ['==', '!=']:
+                return 'bool'
+            else:
+                print("Unsupported operator")
+                return 'error'
+        elif type(type1) == type(type2) == tuple and type1[0] == type2[0] == 'list': #('list','int' or 'float', size x, sizy y)
+            if op in ['.+', '.-', '.*', './']:
+                if type1[2] != type2[2] or type1[3] != type2[3]:
+                    print("Unsupported shape")
+                    return 'error'
+                elif type1[1] == type2[1]:
+                    return type1
+                return ('list', 'float', type1[2], type1[3])
+            elif op == '*':
+                if type1[2] != type2[3]:
+                    print("Unsupported shape")
+                    return 'error'
+                elif type1[1] == type2[1]:
+                    return ('list', type1[1], type1[2], type2[3])
+                return ('list', 'float', type1[2], type2[3])
+            elif op == '/':
+                if type1[2] != type2[2] or type1[3] != type2[3]:
+                    print("Unsupported shape")
+                    return 'error'
+                elif type1[1] == type2[1]:
+                    return ('list', type1[1], type1[2], type1[3])
+                return ('list', 'float', type1[2], type1[3])
+        else:
+            print("Unsupported type")
+            return 'error'
 
     def visit_Variable(self, node):
         pass
